27af16c230349d42e98bf982518041ba9019245d
sophia <scastellarin@quansight.com>
2025-02-21 10:03:59 -0800

    Save hash of lockfile

--- fod/_fod/models/environment.py
+++ fod/_fod/models/environment.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 from pydantic import BaseModel
+import hashlib
 
 from fod._fod.utils import short_uuid
 
@@ -12,9 +13,16 @@ class EnvironmentSpec(BaseModel):
     
     lockifle : str
       The pixi.lock for the environment
+
+    lockfile_hash : str
+      Hash for the content of the lockfile. If the lockfile hash has
+      changed, then the environment has been updated. If the spec
+      file has changed, it will cause a change in the lockfile as
+      well.
     """
     spec: str
     lockfile: str
+    lockfile_hash: str
 
 
 class EnvironmentCheckpoint(BaseModel):
@@ -37,16 +45,17 @@ class EnvironmentCheckpoint(BaseModel):
             raise Exception("did not find pyproject toml")
 
         pixi_lock = ""
-        with open(pixi_lock_path, 'r') as file:
+        with open(pixi_lock_path, 'rb') as file:
             pixi_lock = file.read()
 
         pyproject_toml = ""
-        with open(pyproject_toml_path, 'r') as file:
+        with open(pyproject_toml_path, 'rb') as file:
             pyproject_toml = file.read()
 
         spec = EnvironmentSpec(
             spec = pyproject_toml,
-            lockfile = pixi_lock
+            lockfile = pixi_lock,
+            lockfile_hash = hashlib.sha256(pyproject_toml).hexdigest()
         )
         uuid = short_uuid()
 
